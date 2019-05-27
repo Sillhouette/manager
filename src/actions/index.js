d98b037754010c27d91b1988efdd569630e7b6a7
github username <raustin.melchior@gmail.com>
2019-05-26 17:02:08 -0700

    dispatch action from loginUser

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,9 +12,11 @@ export const passwordChanged = text => ({
   payload: text
 });
 
-export const loginUser = ({ email, password }) => {
+export const loginUser = ({ email, password }) => dispatch => {
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
-    .then(user => console.log(user));
+    .then(user => {
+      dispatch({ type: 'LOGIN_USER_SUCESS', payload: user });
+    });
 };
